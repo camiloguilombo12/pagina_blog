1c0d91b0ae836d9f35586b722882d5363f4b24f0
Juan-Cortes2045 <juanestebanp171@gmail.com>
2026-03-03 10:41:10 -0500

    arreglos en el script y seccion header

--- js/script.js
+++ js/script.js
@@ -10,16 +10,43 @@ function cargarSeccion(ruta, idContenedor, mensajeError) {
 cargarSeccion("components/header.html", "header-container", "Error al cargar el header");
 cargarSeccion("pages/basicas.html", "basicas", "Error al cargar la página de básicas");
 cargarSeccion("pages/texto.html", "texto","Error al cargar la página de texto" );
+cargarSeccion("pages/listas.html", "listas", "Error al cargar la página de listas")
 cargarSeccion("pages/formularios.html", "formularios", "Error al cargar la página de formularios" );
 
 document.addEventListener("DOMContentLoaded", () => {
+ 
+  const secciones = document.querySelectorAll(".seccion");
+
+  function mostrarSeccion(id) {
+    secciones.forEach(sec => {
+      if (sec.id === id) {
+        sec.style.display = "block";
+      } else {
+        sec.style.display = "none";
+      }
+    });
+  }
+
+ 
+  mostrarSeccion("basicas");
+
+
+  document.addEventListener("click", (event) => {
+    const link = event.target.closest("a[data-seccion]");
+    if (!link) return;
+
+    event.preventDefault();
+    const idSeccion = link.getAttribute("data-seccion");
+    mostrarSeccion(idSeccion);
+  });
+
   const formulario = document.getElementById("form-ejemplo");
   const resultado = document.getElementById("resultado-formulario");
 
-  if (!formulario || !resultado) return; // por si esta página no está cargada
+  if (!formulario || !resultado) return;
 
   formulario.addEventListener("submit", (event) => {
-    event.preventDefault(); // evita que la página se recargue
+    event.preventDefault();
 
     const nombre = document.getElementById("nombre").value;
     const correo = document.getElementById("correo").value;
