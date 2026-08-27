40f072405a96ba1fa045a2539217a5e72f2906d0
Juan Camilo Guilombo <cguilombo587@gmail.com>
2026-08-27 16:52:35 -0500

    añadi nginx.conf

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,13 +1,4 @@
-const hostname = window.location.hostname;
-const protocol = window.location.protocol;
-
-let BASE_URL;
-
-if (hostname.includes("killercoda.com")) {
-    BASE_URL = `${protocol}//${hostname.replace("-8081", "-8080")}`;
-} else {
-    BASE_URL = `${protocol}//${hostname}:8080`;
-}
+const BASE_URL = "";
 
 const AUTH_URL = `${BASE_URL}/api/auth`;
 const USUARIOS_URL = `${BASE_URL}/api/usuarios`;
