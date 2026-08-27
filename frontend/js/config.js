26ad0d116cd61a51d950e3365bbef142bc8d0402
Juan Camilo Guilombo <cguilombo587@gmail.com>
2026-08-27 16:19:28 -0500

    configuracion de URL dinamica para el frontend en config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,14 @@
-const BACKEND_PORT = 8080;
-const BASE_URL = `http://${window.location.hostname}:${BACKEND_PORT}`;
+const hostname = window.location.hostname;
+const protocol = window.location.protocol;
+
+let BASE_URL;
+
+if (hostname.includes("killercoda.com")) {
+    BASE_URL = `${protocol}//${hostname.replace("-8081", "-8080")}`;
+} else {
+    BASE_URL = `${protocol}//${hostname}:8080`;
+}
+
 const AUTH_URL = `${BASE_URL}/api/auth`;
 const USUARIOS_URL = `${BASE_URL}/api/usuarios`;
 const FETCH_OPTS_BASE = { credentials: "include" };
